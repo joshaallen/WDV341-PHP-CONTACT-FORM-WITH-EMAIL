bea29ec17348c9506e41ca9f887bad8ffbfb6b99
joshaallen <jaalle10@dmacc.edu>
2021-10-07 18:26:20 -0500

    modified formhandler link attributes

--- formHandler.php
+++ formHandler.php
@@ -32,7 +32,7 @@ $email->sendEmail($mailTo);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Form Handler</title>
-  <link rel="stylesheet" href="../../cssFiles/reset2.css" type="text/css">
+  <link rel="stylesheet" href="reset2.css" type="text/css">
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <!-- jQuery library -->
@@ -41,7 +41,7 @@ $email->sendEmail($mailTo);
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <!-- Load an icon library -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="../../cssFiles/finalProject.css" type="text/css">
+  <link rel="stylesheet" href="finalProject.css" type="text/css">
 </head>
 <body>
 <div class="container">
